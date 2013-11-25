fe7bd123b1612629b9e03e054c78ec4d5e4eec9f
Mike Watson <watsomic@gmail.com>
2013-11-25 10:22:04 -0500

    tweaked js/main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,11 @@
 var app = {
 
-    findByName: function() {
-  	var self = this;
-    	this.store.findByName($('.search-key').val(), function(employees) {
-       	 $('.employee-list').html(self.employeeLiTpl(employees));
-      });
-	},
-
-    showAlert: function (message, title) {
-       if (navigator.notification) {
-          navigator.notification.alert(message, null, title, 'OK');
-       } else {
-          alert(title ? (title + ": " + message) : message);
-       }
-},
-
-renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
-
 initialize: function() {
     var self = this;
-    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());	
     this.store = new MemoryStore(function() {
-        self.renderHomeView();
+    	$('body').html(new HomeView(self.store).render().el);
     });
 }
-  /*initialize: function() {
-        this.store = new MemoryStore();
-	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }*/
-
 
 };
 
